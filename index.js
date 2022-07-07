94e22c5692104e9d36d2e3477db6a54955f598f1
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-07-07 09:33:04 -0700

    commit tree c32b1df366f53fc09d100d80d233856685312d64

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded")
+    coolMessage();
+})
+
+const coolMessage = function(e){
+    const whateverText = document.getElementById("text") 
+    whateverText.textContent = 'This is really cool!'
+}
+
